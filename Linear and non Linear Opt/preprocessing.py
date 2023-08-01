ee4e552dc8ae1c436b1c4ca8ce7700af512d937a
Paolo D'Elia <paolo.delia99@gmail.com>
2023-08-01 23:37:11 +0200

    Implemented the price preprocessing in the pre.py

--- Linear and non Linear Opt/preprocessing.py
+++ Linear and non Linear Opt/preprocessing.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isfile, join
 import argparse
 
+import numpy as np
 import pandas as pd
 
 def parse_args():
@@ -38,10 +39,45 @@ def preprocess_dk_stock_data():
 
     return close_df
 
+def stock_data_preprocessing(close_df: pd.DataFrame) -> None:
+    def get_weekly_log_returns(df_weekly_prices: pd.DataFrame) -> pd.DataFrame:
+        df_weekly_log_returns = np.log(df_weekly_prices) - np.log(df_weekly_prices.shift(1))
+        df_weekly_log_returns = df_weekly_log_returns.dropna(how='all')
+        df_weekly_log_returns = df_weekly_log_returns.fillna(0)
+        return df_weekly_log_returns
+
+    close_df = close_df.fillna(method='bfill')
+    close_df.index = pd.to_datetime(close_df.index)
+
+    df_weekly_prices = close_df.resample('W').last()
+
+    df_weekly_log_returns = get_weekly_log_returns(df_weekly_prices)
+
+    return_array = df_weekly_log_returns.to_numpy()
+    m_weekly_log = np.mean(return_array, axis=0)
+    S_weekly_log = np.cov(return_array.transpose())
+
+    m_log = 52 * m_weekly_log
+    S_log = 52 * S_weekly_log
+
+    p_0 = df_weekly_prices.iloc[0].to_numpy()
+    m_P = p_0 * np.exp(m_log + 1/2*np.diag(S_log))
+    S_P = np.outer(m_P, m_P) * (np.exp(S_log) - 1)
+
+    m = 1 / p_0 * m_P - 1
+    S = 1 / np.outer(p_0, p_0) * S_P
+
+    np.save('..\data\DK\preprocessed_data\stocks\m_log.npy', m_log)
+    np.save('..\data\DK\preprocessed_data\stocks\S_log.npy', S_log)
+    np.save('..\data\DK\preprocessed_data\stocks\m.npy', m)
+    np.save('..\data\DK\preprocessed_data\stocks\S.npy', S)
+
+
 if __name__ == '__main__':
     args = parse_args()
 
     if args.nation == 'DK':
         if args.instument == 'stock':
             preprocessed_df = preprocess_dk_stock_data()
+            stock_data_preprocessing(preprocessed_df)
             preprocessed_df.to_csv('..\data\DK\preprocessed_data\danish_closed_stocks.csv')
